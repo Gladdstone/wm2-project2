f8b9375c49bd48918156a92cb32d963ad1434369
Joe Farrell <jdf4921@rit.edu>
2017-12-08 17:07:50 -0500

    Mobile dropdown function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,25 @@
+/**
+ * File:
+ * Author:
+ * Description:
+ */
+
+/**
+ * Variable declaration
+ */
+var countyClosed = true,
+	pointsClosed = true;
+
+
+function closeNav() {
+    // Clean up open dropdowns
+    document.getElementById("mobile_county").style.height = "0";
+    countyClosed = true;
+    // Close menu
+    document.getElementById("mobile_menu_button").classList.toggle("change");
+    document.getElementById("mobilemenu").style.width = "0";
+}
+
 function dropdownInterest() {
 	document.getElementById("dropdown_interest").classList.toggle("show");
 }
@@ -7,24 +29,45 @@ function dropdownRegions() {
 }
 
 function openMenu(elem) {
-	elem.classList.toggle("change");
-	openNav()
+    elem.classList.toggle("change");
+    openNav()
 }
 
 function openNav() {
-	document.getElementById("mobilemenu").style.width = "250px";
-}
-
-function closeNav() {
-	document.getElementById("mobile_menu_button").classList.toggle("change");
-	document.getElementById("mobilemenu").style.width = "0";
+    document.getElementById("mobilemenu").style.width = "280px";
 }
 
 function setYear() {
-	alert("test");
-	var today = new Date();
-	var year = today.getFullYear();
+    alert("test");
+    var today = new Date();
+    var year = today.getFullYear();
 
-	var text = document.getElementById("copyright").innerHTML;
-	document.getElementById("copyright").innerText = text + " " + year;
+    var text = document.getElementById("copyright").innerHTML;
+    document.getElementById("copyright").innerText = text + " " + year;
+}
+
+function toggleCounty() {
+	if(countyClosed) {
+        document.getElementById("mobile_county").style.height = "350px";
+        document.getElementById("county_arrow").innerHTML = "&#x25BC;";
+        countyClosed = false;
+    }
+    else {
+        document.getElementById("mobile_county").style.height = "0";
+        document.getElementById("county_arrow").innerHTML = "&#x25B2;";
+        countyClosed = true;
+	}
+}
+
+function togglePoints() {
+	if(pointsClosed) {
+		document.getElementById("mobile_points").style.height = "250px";
+        document.getElementById("points_arrow").innerHTML = "&#x25BC;";
+		pointsClosed = false;
+	}
+	else {
+		document.getElementById("mobile_points").style.height = "0";
+        document.getElementById("points_arrow").innerHTML = "&#x25B2;";
+		pointsClosed = true;
+	}
 }
