69f4729b610ab7b3a628c8ef27ffd5fecb9ca318
Joe Farrell <jdf4921@rit.edu>
2017-12-09 17:45:57 -0500

    About page

---
+++ about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title></title>
+    <meta name="description" content="">
+    <meta name="author" content="Joseph Farrell">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php include "assets/media.html" ?>
+</head>
+<body>
+<div id="content">
+    <?php include "assets/navbar.html" ?>
+    <br/><br/>
+    <h1>About</h1>
+    <hr>
+    <br/>
+    <h3>Changes in the latest version: </h3>
+    <div class="fltlft">
+        <ul>
+            <li>Slide-out menu with onclick dropdowns for large submenus</li>
+            <li>Mobile optimization for individual county pages</li>
+            <li>Media queries moved to a separate file included by server</li>
+            <li>Custom CSS file for use on matching media query</li>
+        </ul>
+    </div>
+</div>
+<?php include "assets/footer.html" ?>
+<script type="text/javascript" src="assets/js/scripts.js"></script>
+</body>
+</html>
