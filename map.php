46358be0eb53ec0d93f1a56fdcc58d05729f75a8
Joseph Daniel Farrell <jdf4921@banjo.rit.edu>
2017-12-09 12:16:49 -0500

    Map page working and formatted to LA

--- map.php
+++ map.php
@@ -15,12 +15,17 @@
 	<div id="content">
 		<?php include "assets/navbar.html" ?>
 		<br/><br/>
-		<iframe
-			width="600"
-			height="450"
-			frameborder="0" style="border:0"
-			src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDlMmWUvoNg0q0-0k4vtyNKFnWrZ9R1rGI&q=Space+Needle,Seattle+WA" allowfullscreen>
-		</iframe>
+		<h1>Southern California - Map</h1>
+		</hr>
+		<br/><br/>
+		<div id="map">
+			<iframe
+				width="600"
+				height="450"
+				frameborder="0" style="border:0"
+				src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDlMmWUvoNg0q0-0k4vtyNKFnWrZ9R1rGI&q=Los+Angeles,CA" allowfullscreen>
+			</iframe>
+		</div>
 	</div>
     <?php include "assets/footer.html" ?>
     <script src="assets/js/scripts.js"></script>
